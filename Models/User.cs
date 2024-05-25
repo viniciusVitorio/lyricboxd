0af1eaf1a964c3c8122fefc7a96b0034a6f94bc9
Lucas Zambam <lucaszambam7896@gmail.com>
2024-05-24 21:55:38 -0300

    initial commit

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lyricboxd.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(60)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(120)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(225)]
+        public string Password { get; set; }
+    }
+}
