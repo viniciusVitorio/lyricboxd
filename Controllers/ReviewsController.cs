ce43524d50d1afb0d51e25e1c09ae908587009de
Vinicius Vitório <vini.vtj@gmail.com>
2024-07-04 21:46:41 -0300

    Add namespace

--- Controllers/ReviewsController.cs
+++ Controllers/ReviewsController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
 namespace lyricboxd.Controllers
 {
